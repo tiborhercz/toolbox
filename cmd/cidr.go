856476715bb116908d5023aa5c6288e1c90e1ea9
tiborhercz <tibor@hercz.nl>
2021-11-25 19:18:09 +0100

    update command descriptions

--- cmd/cidr.go
+++ cmd/cidr.go
@@ -20,6 +20,6 @@ var (
 
 func init() {
 	rootCmd.AddCommand(cidrCmd)
-	cidrCmd.Flags().StringVarP(&cidrOptions.IpAddress, "ipaddress", "i", "", "ip address")
-	cidrCmd.Flags().Int64VarP(&cidrOptions.IpCidrPrefix, "cidrprefix", "c", 64, "IpCidrPrefix default 64")
+	cidrCmd.Flags().StringVarP(&cidrOptions.IpAddress, "ipaddress", "i", "", "ip address. Example inputs: 10.0.0.0/16 or 2001:db8:3333:4444:5555:6666:7777:8888/64")
+	cidrCmd.Flags().Int64VarP(&cidrOptions.IpCidrPrefix, "cidrprefix", "c", 64, "IpCidrPrefix default 64. Only applies to IPv6")
 }

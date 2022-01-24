01921d85a2471d06e63813551ce05caca75540ae
tiborhercz <tibor@hercz.nl>
2022-01-24 08:53:11 +0100

    improve commands. Replace flags with arguments

--- cmd/cidr.go
+++ cmd/cidr.go
@@ -1,31 +1,43 @@
 package cmd
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/tiborhercz/cli-toolbox/internal/cidr"
-	"github.com/tiborhercz/cli-toolbox/internal/model"
 )
 
 var (
-	cidrOptions model.CidrOptions
-
 	cidrCmd = &cobra.Command{
 		Use:   "cidr",
 		Short: "Calculate IPv4 CIDR ranges",
+	}
+
+	cidrIpAddressCmd = &cobra.Command{
+		Use:   "ip",
+		Short: "Calculate IPv4 CIDR ranges from IP address",
 		Run: func(cmd *cobra.Command, args []string) {
-			if cidrOptions.IpAddress != "" {
-				cidr.IpAddress(cidrOptions.IpAddress)
+			if len(args) < 1 {
+				logrus.Fatal("requires an argument. Example argument: 10.0.0.0/16")
 			}
 
-			if cidrOptions.SubnetMaskAddress != "" {
-				cidr.SubnetMask(cidrOptions.SubnetMaskAddress)
+			cidr.IpAddress(args[0])
+		},
+	}
+
+	cidrSubnetMaskCmd = &cobra.Command{
+		Use:   "subnetmask",
+		Short: "Calculate IPv4 CIDR ranges from subnetmask",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				logrus.Fatal("requires an argument. Example argument: 255.255.255.0")
 			}
+
+			cidr.SubnetMask(args[0])
 		},
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(cidrCmd)
-	cidrCmd.Flags().StringVarP(&cidrOptions.IpAddress, "ipaddress", "i", "", "ip address. Example input: 10.0.0.0/16")
-	cidrCmd.Flags().StringVarP(&cidrOptions.SubnetMaskAddress, "subnetmask", "s", "", "Subnetmask address. Example input: 255.255.255.0")
+	cidrCmd.AddCommand(cidrIpAddressCmd, cidrSubnetMaskCmd)
 }

ce95cca8c55fad23ad12dfa48d4a3c937a9a204e
tiborhercz <tibor@hercz.nl>
2022-01-24 08:53:11 +0100

    improve commands. Replace flags with arguments

--- cmd/jwtdecode.go
+++ cmd/jwtdecode.go
@@ -5,19 +5,20 @@ import (
 	"encoding/json"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/tiborhercz/cli-toolbox/internal/model"
 	"github.com/tiborhercz/cli-toolbox/pkg/jwtdecode"
 	"log"
 )
 
 var (
-	jwtOptions model.JwtOptions
-
 	jwtdecodeCmd = &cobra.Command{
 		Use:   "jwtdecode",
 		Short: "Decode jwt token",
 		Run: func(cmd *cobra.Command, args []string) {
-			jwtData, err := jwtdecode.Process(jwtOptions.Value)
+			if len(args) < 1 {
+				logrus.Fatal("requires an argument. Example argument: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+			}
+
+			jwtData, err := jwtdecode.Process(args[0])
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -32,7 +33,6 @@ var (
 
 func init() {
 	rootCmd.AddCommand(jwtdecodeCmd)
-	jwtdecodeCmd.Flags().StringVarP(&jwtOptions.Value, "value", "v", "", "Value string")
 }
 
 func prettifyJson(value []byte) (string, error) {
--- internal/model/options.go
+++ internal/model/options.go
@@ -1,20 +1,9 @@
 package model
 
 type Base64Options struct {
-	Value       string
 	Path        string
 	Decode      bool
 	Urlencoding bool
-}
-
-type JwtOptions struct {
-	Value string
-}
-
-type CidrOptions struct {
-	IpAddress         string
-	PrefixLength      int64
-	SubnetMaskAddress string
 }
 
 type PasswordOptions struct {

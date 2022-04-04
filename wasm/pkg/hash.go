e2c77a8747f91820cbada648246b9596a81cf97c
tiborhercz <tibor@hercz.nl>
2022-04-04 21:40:20 +0200

    add hash ui

---
+++ wasm/pkg/hash.go
@@ -0,0 +1,27 @@
+package pkg
+
+import (
+	"fmt"
+	"github.com/tiborhercz/cli-toolbox/pkg/hash"
+	"syscall/js"
+)
+
+func GetSupportedHashingAlgorithms(this js.Value, args []js.Value) any {
+	var algorithms = hash.GetSupportedHashingAlgorithms()
+
+	a := make([]interface{}, len(algorithms))
+	for i, v := range algorithms {
+		a[i] = v
+	}
+
+	return a
+}
+
+func Hash(this js.Value, args []js.Value) any {
+	hash, err := hash.Execute([]byte(args[0].String()), args[1].String())
+	if err != nil {
+		return fmt.Sprint(err)
+	}
+
+	return hash
+}

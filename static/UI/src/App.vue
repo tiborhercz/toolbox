996e5b0da3cede9d29922eb94924b4b7c6ab08a2
tiborhercz <tibor@hercz.nl>
2022-04-05 10:12:46 +0200

    dark mode toggle consistent on refresh

--- static/UI/src/App.vue
+++ static/UI/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-app v-bind:style="{background: $vuetify.theme.themes['dark'].background}">
+  <v-app v-bind:style="{background: $vuetify.theme.themes[darkTheme].background}">
     <v-navigation-drawer
-      v-bind:color="$vuetify.theme.themes['dark'].drawer"
+      v-bind:color="$vuetify.theme.themes[darkTheme].drawer"
       app
     >
       <v-container>
@@ -39,14 +39,27 @@ export default {
   data: () => ({
     darkMode: true,
   }),
+  computed: {
+    darkTheme() {
+      return this.darkMode ? 'dark' : 'light'
+    },
+  },
+  mounted() {
+    const theme = localStorage.getItem('dark_theme')
+    if (theme) {
+      this.$vuetify.theme.dark = theme === 'true'
+      this.darkMode = theme === 'true'
+    }
+  },
   methods: {
     toggleDarkMode() {
       if (this.darkMode) {
-        window.document.body.style.backgroundColor = '#121212'
+        window.document.body.style.backgroundColor = '#222222'
       } else {
         window.document.body.style.backgroundColor = '#ffffff'
       }
       this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+      localStorage.setItem('dark_theme', this.$vuetify.theme.dark.toString())
     },
   },
 }
